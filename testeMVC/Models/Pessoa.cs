cd97c857f20b1e0045835d711f9b05061ec32166
belafrata <fratabela@gmail.com>
2025-05-27 21:20:18 -0300

    Criando classe Pessoa

---
+++ testeMVC/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace testeMVC.Models
+{
+    public class Pessoa
+    {
+        public int? PessoaId { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+    }
+}
